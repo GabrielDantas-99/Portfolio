b216fcdcd92139e2fee50c193ec07b69792dd951
Gabriel Dantas <gabrieldantaslp@gmail.com>
2022-04-16 00:31:19 -0300

    Header inicial

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Olá, Eu sou</h5>
+        <h1>Gabriel Dantas</h1>
+        <h5 class="text-light">Desenvolvedor Web</h5>
+        <CTA/>
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
